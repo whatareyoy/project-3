885e3f6f1727e13b3fcbe4296a896d779ef34662
Jorge Martinez <113647218+whatareyoy@users.noreply.github.com>
2023-04-28 21:29:37 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.76, lng: -73.983 },
-    zoom: 15,
-    mapTypeId: "satellite",
-  });
-
-  map.setTilt(45);
+  const map = new google.maps.Map(document.getElementById("map");
+    var myLocation = new google.maps.LatLng(41.831319, -87.627133);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 20,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
 }
-
+	}
+  });
+ const marker = new google.maps.Marker({
+  position: {lat: 41.831319, lng: -87.627133},
+  title: "IIT Tower",
+  animation: google.maps.Animation.Drop,
+  icon: "src="images/saul.jpg"
+ });
+  const infoWindow = new google.maps.InfoWindow({
+	  constent: "<p>IIT Tower<p>"
+ });
+infoWindow.open(Marker);
+}
 window.initMap = initMap;

176e1d96f3cff16859df4b6475cd318d802d1c80
Jorge Martinez <113647218+whatareyoy@users.noreply.github.com>
2023-05-01 02:21:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map");
-    var myLocation = new google.maps.LatLng(41.831319, -87.627133);
-	var mapOptions = {
-		center: myLocation,
-		zoom: 20,
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.BOTTOM_CENTER
+	var IITtower = {
+	center: {lat: 41.831319, lng: -87.627133},
+	zoom: 16,
 }
-	}
-  });
- const marker = new google.maps.Marker({
-  position: {lat: 41.831319, lng: -87.627133},
-  title: "IIT Tower",
-  animation: google.maps.Animation.Drop,
-  icon: "src="images/saul.jpg"
+map = new google.maps.Map(document.getElementById("map"),IITtower)
+
+
+	const marker = new google.maps.Marker({
+	position: {lat: 41.831319, lng: -87.627133},
+	title: "IIT Tower",
+	map:map,
  });
-  const infoWindow = new google.maps.InfoWindow({
-	  constent: "<p>IIT Tower<p>"
- });
-infoWindow.open(Marker);
+ var infoWindow = new google.maps.InfoWindow({
+      constent:'<h3>IIT Tower<h3>'
+});
+	marker.addListener('click',function(){
+		infoWindow.open(map, marker);
+	});
 }
-window.initMap = initMap;

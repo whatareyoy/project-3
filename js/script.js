96b70e513fc39f9a9f2879dbfa8002502efdf6a0
Jorge Martinez <113647218+whatareyoy@users.noreply.github.com>
2023-05-01 14:55:41 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ function initMap() {
 }
 map = new google.maps.Map(document.getElementById("map"),IITtower)
 
-
-	const marker = new google.maps.Marker({
+	var marker = new google.maps.Marker({
 	position: {lat: 41.831319, lng: -87.627133},
 	title: "IIT Tower",
 	map:map,
+	animation: google.maps.Animation.DROP
  });
- var infoWindow = new google.maps.InfoWindow({
-      constent:'<h3>IIT Tower<h3>'
+	
+const infoWindow = new google.maps.InfoWindow({
+	content: "<p>The IIT Tower was build in 1964.<p>"
 });
-	marker.addListener('click',function(){
-		infoWindow.open(map, marker);
-	});
+infoWindow.open(map,marker);
 }
+window.initMap = initMap;
